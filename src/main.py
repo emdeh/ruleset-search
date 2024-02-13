8af9ee041e72b39b7dc2e5c755c6f5151ed820b2
emdeh <emdeh-cyber@proton.me>
2024-02-13 20:02:32 +1100

    comment cleaning in main.py

--- src/main.py
+++ src/main.py
@@ -1,62 +1,64 @@
-# Description: This script demonstrates how to process text from Azure Blob Storage and write the results to a CSV file.
+# Description: This script processes JSON data from Azure Blob Storage and write the results to a CSV file.
 import sys
 import os
 from text_processing import write_file_manifest, process_text, load_rulesets
 from azure_blob_utils import get_blob_service_client, list_blobs, read_blob_content
 from azure.storage.blob import BlobServiceClient
 import csv
-import json # for testing
+# import json # for testing
 
-# Calculate the absolute path to the project root
+# Calculate the absolute path to the project root.
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-# Add the project root to the system path
+# Add the project root to the system path.
 sys.path.insert(0, project_root)
 
-# import app_settings
+# Import app_settings.
 from config import app_settings
 
 def main():
     
-    # Initialize the Azure Blob Service Client
+    # Initialize the Azure Blob Service Client.
     blob_service_client = get_blob_service_client(app_settings.blob_account_url, app_settings.blob_credential)
     print(f"Successfully connected to the Azure Blob Storage account: {app_settings.blob_account_url} with service client: {blob_service_client}")
 
-    # Define the path prefix for the subfolder structure
+    # Define the path prefix for the subfolder structure in the Blob container.
     path_prefix = app_settings.blob_path_prefix
     print(f"Processing blobs in the container with path prefix: {path_prefix}")
 
-    # Create manifest of input files
+    # Create manifest of input files from the blob names.
     write_file_manifest(blob_service_client, app_settings.blob_container_name, path_prefix, app_settings.output_dir, 'manifest.txt')
 
-    # Load the rulesets from a JSON file
+    # Load the rulesets from a JSON file.
     rulesets = load_rulesets(app_settings.ruleset_path)   
 
-    # Prepare the CSV file for output
+    # Prepare the CSV file for output with headers.
     with open(app_settings.output_csv_path, 'w', newline='', encoding='utf-8') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['File Name', 'Triggered Rule', 'Triggered Condition', 'Surrounding context (Extracts the the sentences immediately before and after where the condition was triggered)'])
+        csvwriter.writerow(['File Name', 'Triggered Rule', 'Triggered Condition', 'Surrounding context (Extracts the the sentences immediately before and after where the condition was triggered)'])      
 
-        
-
-        # Process each blob in the container
+        # Process each blob in the container.
         print(f"Listing blobs in container: {app_settings.blob_container_name}")
         for blob in list_blobs(blob_service_client, app_settings.blob_container_name, path_prefix):
             print(f"Found blob: {blob.name}")
             blob_name = blob.name
             content = read_blob_content(blob_service_client, app_settings.blob_container_name, blob_name)
+            print(f"Read content for {blob_name}")
     
             if 'text_content' in content and content['text_content'].strip():
-            # 'strip()' removes leading/trailing whitespace, making this check fail for empty or whitespace-only content
+            # Check if key 'text_content' exists AND is not empty or whitespace-only.
+            # 'strip()' removes leading/trailing whitespace, making this check fail for     empty or whitespace-only content.
                 text_content = content['text_content']
                 print(f"Processing blob: {blob_name} with text content length: {len(text_content)}")
             
                 # Process the text content
-                clickable_path = app_settings.original_path + blob_name
-                extracted_info = process_text(text_content, rulesets)
+                # clickable_path = app_settings.original_path + blob_name # TODO: Add clickable path to the output
+
+                extracted_info = process_text(text_content, rulesets) 
+                # Calls the process_text function from text_processing.py, that uses flashtext to extract information based on rulesets, and also uses nltk to tokenize the text into sentences.
                 for match in extracted_info:
 
-                    # Compile the surrounding context for the current match in a list.
+                    # Use list comprehension to compile the surrounding context for the current match in a list.
                     surrounding_context = [s for s in [match["prev_sentence"], match["sentence"], match["next_sentence"]] if s]
 
                     # Concatenate the surrounding context and strip any leading/trailing whitespace
